f2219c0b8bbd248f0f911802b74055fa3cafc2e5
Dan Haddigan <haddigan@gmail.com>
2020-09-20 11:57:03 -0400

    wrap children in a container with margin

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,9 @@ import Navigation from "../components/Navigation";
 import Header from "../components/Header";
 
 export default ({ children }) => (
-  <div className="lg:container lg:mx-auto px-4">
+  <div className="lg:container lg:mx-auto px-4 font-sans">
     <Header />
     <Navigation />
-    {children}
+    <section className="my-8">{children}</section>
   </div>
 );
