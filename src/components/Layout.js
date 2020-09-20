14de93d28279090ceb4e3b872795250fc7c16481
Dan Haddigan <haddigan@gmail.com>
2020-09-20 12:30:30 -0400

    move css import to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import Header from "../components/Header";
-
+import "../css/index.css";
 export default ({ children }) => (
   <div className="lg:container lg:mx-auto px-4 font-sans">
     <Header />
