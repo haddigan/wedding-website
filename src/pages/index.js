7d53c381124ddea868a90cc8a32b6aff266b7515
Dan Haddigan <haddigan@gmail.com>
2020-09-20 12:30:53 -0400

    put content on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react";
-import "../css/index.css";
-import Layout from "../components/Layout";
 
-export default () => <Layout>Coming soon</Layout>;
+import Layout from "../components/Layout";
+import Photo from "../../images/dan+siobhan.jpg";
+
+export default () => (
+  <Layout>
+    <h1 className="font-medium text-center text-2xl">We're Getting Married!</h1>
+    <h2 className="font-light text-center text-l">
+      Saturday, September 26, 2020
+    </h2>
+    <p className="my-8 flex-1">
+      <img
+        src={Photo}
+        alt="Dan and Siobhan (and Bert)"
+        className="px-8 object-contain"
+      />
+    </p>
+  </Layout>
+);
