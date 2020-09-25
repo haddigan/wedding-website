b518568f9f48971d878ae0e0d325bdfd055fc253
Dan Haddigan <haddigan@gmail.com>
2020-09-25 09:58:07 -0400

    add background color to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,9 +3,16 @@ import Navigation from "../components/Navigation";
 import Header from "../components/Header";
 import "../css/index.css";
 export default ({ children }) => (
-  <div className="lg:container lg:mx-auto px-4 font-sans">
-    <Header />
-    <Navigation />
-    <section className="my-8">{children}</section>
+  <div
+    className="outer md:py-4"
+    style={{
+      backgroundColor: "#616f43",
+    }}
+  >
+    <div className="lg:container lg:mx-auto px-4 pb-4 font-sans bg-white">
+      <Header />
+      <Navigation />
+      <section className="my-8">{children}</section>
+    </div>
   </div>
 );
